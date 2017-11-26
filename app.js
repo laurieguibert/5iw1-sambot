4099fa0ec9c529a6c104a57c8227f684ca9dfb44
Manon <deschamps.manon.91@gmail.com>
2017-11-26 23:43:10 +0100

    purpose events categories

--- app.js
+++ app.js
@@ -30,6 +30,33 @@ bot.recognizer(recognizer);
 var start_url = 'https://www.eventbriteapi.com/v3/'
 var user_token = '6KAACOLJPUJMIKMB43E7'
 
+function create_event_card(session, response) {
+    var msg = new botbuilder.Message(session);
+    msg.attachmentLayout(botbuilder.AttachmentLayout.carousel)
+    events_array = [] 
+    response.data.events.forEach(function(value){
+        var thumbnail_url
+        if ( typeof value.logo !== 'undefined' && value.logo )
+        {
+            thumbnail_url = value.logo.url
+        }
+        else
+        {
+            thumbnail_url = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPoAAACsCAMAAABl5UHlAAAAFVBMVEXd3d3MzMzZ2dnR0dHV1dXX19fS0tJ77b4nAAACDUlEQVR4nO3c0ZaCIABFUQPk/z95XJYEpAIWOd7Ofq0HzoiMYTYMAAAAAAAAAAAAAAAAAAAAAAAAmDhbxZmzB/px9lZtPHusn+Xry283e/ZoP6qlfKI06xvTldrnHu92ibbX5Nzf49Ta69OHUay9IX0wWu0t6WLtTela7W3pUu2N6UrtrelC7c3pOu3t6TLtB9JDe9+RdXckfWn3XUfW3aH0pb3nwPo7lv5odx0H1l91ujOJeV/r2rs21emrvjPGTkjftb11+Z0xdlKTbn43fTBbx/0rQ+wlS3fTym1r/mcZsfSw91qOF0uPpnXxGlUrPdlvLx13rfSmFUwqfUzTC7dVpdLT+0uls10qPbvRXvhoIpXufzc9m/CFJV4qPVvhk4vb17+DVnoy45P57l+nv1Z6ctjj9/iVU18sPfpwGk93v7bsiaWHpS45tZfzIG2XS5+SXPbdwOcKkLQLpufitS9u109Pr3Oidvn0fGPq2a6e/rolF9rF09c2I5d2xXQbDuz6NuzjZcF0G+K2NqDvL+ulP+8ibt9ymS945NJtOLA735OXTA8bNXbvCQHF9MpnQQTTa5+C0UuvfgpGL7360SfSSb8u0gfSSf+x9DakXxnpzUi/srmg7icbUiLph509+re8VX7tRwHGcuC2i/9uhysXipZPzEFnjxsAAAAAAAAAAAAAAAAAAAAAAPwvf+8bDRHr18+cAAAAAElFTkSuQmCC'
+        }
+        events_array.push(
+            new botbuilder.HeroCard(session)
+            .title(truncate(value.description.text, 38))
+            .subtitle(dateFormat(value.start.utc, "dddd, mmmm dS yyyy, h:MM TT") +", "+ value.start.timezone )
+            .text(truncate(value.description.text, 300))
+            .images([botbuilder.CardImage.create(session, thumbnail_url)])
+            .buttons([botbuilder.CardAction.openUrl(session, value.url, "Read More")])
+        );
+    });
+    msg.attachments(events_array);
+    session.send(msg).endDialog();
+}
+
 bot.dialog('Greeting', [
     function (session, args, next) {
         axios.get(start_url + 'users/me/?token=' + user_token)
@@ -53,31 +80,7 @@ bot.dialog('Events', [
         var event_keyword = event_keyword_entity ? '&q='+event_keyword_entity.entity : '';
         axios.get(start_url + 'events/search/?token=' +  user_token + "&sort_by=date" + event_place + event_keyword)
         .then(response => {
-            var msg = new botbuilder.Message(session);
-            msg.attachmentLayout(botbuilder.AttachmentLayout.carousel)
-            events_array = [] 
-            response.data.events.forEach(function(value){
-                var thumbnail_url
-                if ( typeof value.logo !== 'undefined' && value.logo )
-                {
-                    thumbnail_url = value.logo.url
-                }
-                else
-                {
-                    thumbnail_url = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPoAAACsCAMAAABl5UHlAAAAFVBMVEXd3d3MzMzZ2dnR0dHV1dXX19fS0tJ77b4nAAACDUlEQVR4nO3c0ZaCIABFUQPk/z95XJYEpAIWOd7Ofq0HzoiMYTYMAAAAAAAAAAAAAAAAAAAAAAAAmDhbxZmzB/px9lZtPHusn+Xry283e/ZoP6qlfKI06xvTldrnHu92ibbX5Nzf49Ta69OHUay9IX0wWu0t6WLtTela7W3pUu2N6UrtrelC7c3pOu3t6TLtB9JDe9+RdXckfWn3XUfW3aH0pb3nwPo7lv5odx0H1l91ujOJeV/r2rs21emrvjPGTkjftb11+Z0xdlKTbn43fTBbx/0rQ+wlS3fTym1r/mcZsfSw91qOF0uPpnXxGlUrPdlvLx13rfSmFUwqfUzTC7dVpdLT+0uls10qPbvRXvhoIpXufzc9m/CFJV4qPVvhk4vb17+DVnoy45P57l+nv1Z6ctjj9/iVU18sPfpwGk93v7bsiaWHpS45tZfzIG2XS5+SXPbdwOcKkLQLpufitS9u109Pr3Oidvn0fGPq2a6e/rolF9rF09c2I5d2xXQbDuz6NuzjZcF0G+K2NqDvL+ulP+8ibt9ymS945NJtOLA735OXTA8bNXbvCQHF9MpnQQTTa5+C0UuvfgpGL7360SfSSb8u0gfSSf+x9DakXxnpzUi/srmg7icbUiLph509+re8VX7tRwHGcuC2i/9uhysXipZPzEFnjxsAAAAAAAAAAAAAAAAAAAAAAPwvf+8bDRHr18+cAAAAAElFTkSuQmCC'
-                }
-                console.log(value.logo)
-                events_array.push(
-                    new botbuilder.HeroCard(session)
-                    .title(truncate(value.description.text, 38))
-                    .subtitle(dateFormat(value.start.utc, "dddd, mmmm dS yyyy, h:MM TT") +", "+ value.start.timezone )
-                    .text(truncate(value.description.text, 300))
-                    .images([botbuilder.CardImage.create(session, thumbnail_url)])
-                    .buttons([botbuilder.CardAction.openUrl(session, value.url, "Read More")])
-                );
-            });
-            msg.attachments(events_array);
-            session.send(msg).endDialog();
+            create_event_card(session, response);
         })
         .catch(error => {
           console.log("err: "+ error);
@@ -85,4 +88,36 @@ bot.dialog('Events', [
     }
 ]).triggerAction({
     matches: 'Events'
+});
+
+var categories_hash = {};
+bot.dialog('Categories', [
+    function (session, args) {
+        axios.get(start_url + 'categories/?token=' +  user_token)
+        .then(response => {
+            response.data.categories.forEach(function(value){
+                categories_hash[value.name] = {id: value.id}
+            });
+            botbuilder.Prompts.choice(session, "Which kind of event could interest you ?", categories_hash, { listStyle: botbuilder.ListStyle.button });
+        })
+        .catch(error => {
+          console.log("err: "+ error);
+        });
+    },
+    function (session, results) {
+        if (results.response) {
+            var category = categories_hash[results.response.entity];
+            axios.get(start_url + 'events/search/?token=' +  user_token + "&categories=" + category.id)
+            .then(response => {
+                create_event_card(session, response);
+            })
+            .catch(error => {
+              console.log("err: "+ error);
+            });
+
+            session.send(); 
+        }
+    }
+]).triggerAction({
+    matches: 'Categories'
 });

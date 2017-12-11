53010f10580def6ff8088be10a5367497dbfdd4d
Laurie <l.guibert@hotmail.fr>
2017-12-11 14:32:26 +0100

    add EventBrite authentication

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const botbuilder = require('botbuilder');
 const axios = require('axios');
 const truncate = require('truncate');
 const dateFormat = require('dateformat');
+const  mysql = require('mysql');
+require('dotenv').config({path: 'config.env'});
 
 // Setup restify server
 const server = restify.createServer();
@@ -16,19 +18,26 @@ const connector = new botbuilder.ChatConnector({
     appPassword: process.env.APP_SECRET
 });
 
+const database_connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+});
+
 // Listening for user input
 server.post('/api/messages', connector.listen());
 
 var bot = new botbuilder.UniversalBot(connector, function(session){
-    session.send("Hmmm.. I didn't understand that. Can you say it differently") 
+    session.send("Hmmm.. I didn't understand that. Can you say it differently");
 });
 
-const luisEndpoint = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/23f9a52a-8a5e-41bd-8d14-95bba2f4b940?subscription-key=57376ff9997147ba877ca8894ddd3dbb&verbose=true&timezoneOffset=0&q=';
+const luisEndpoint = process.env.LUIS_ENDPOINT;
 var recognizer = new botbuilder.LuisRecognizer(luisEndpoint);
 bot.recognizer(recognizer);
 
 var start_url = 'https://www.eventbriteapi.com/v3/'
-var user_token = '6KAACOLJPUJMIKMB43E7'
 
 function create_event_card(session, response) {
     var msg = new botbuilder.Message(session);
@@ -57,16 +66,50 @@ function create_event_card(session, response) {
     session.send(msg).endDialog();
 }
 
+function say_hello(session) {
+    axios.get(start_url + 'users/me/?token=' + session.userData.token)
+    .then(response => {
+        session.send('Hi '+ response.data.name +', nice to see you');
+        session.endDialog("My name is Sambot, I'm here to help you to find an idea of activity. What can I do for you ?").endDialog();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+bot.dialog('Login', [
+    function(session) { 
+        var msg = new botbuilder.Message(session) 
+        .attachments([ 
+            new botbuilder.SigninCard(session) 
+                .text("Authorization needed") 
+                .button("Login", "https://www.eventbrite.com/oauth/authorize?response_type=code&client_id="+process.env.EVENTBRITE_CLIENT_ID) 
+        ]); 
+        botbuilder.Prompts.text(session, msg);
+    },
+    function(session, results) {
+        database_connection.connect(function(err) {
+            if (err) throw err;
+            database_connection.query("SELECT hash FROM tokens WHERE code = '"+results.response+"'", function (err, result, fields) {
+                if (err) throw err;
+                if (result.length > 0) {
+                    session.userData.token = result[0].hash
+                    say_hello(session);
+                } else {
+                    session.send("Your code looks to be wrong, please try with an other code");
+                }
+            });
+        });
+    } 
+]);
+
 bot.dialog('Greeting', [
     function (session, args, next) {
-        axios.get(start_url + 'users/me/?token=' + user_token)
-        .then(response => {
-            session.send('Hi '+ response.data.name +', nice to see you');
-            session.endDialog("My name is Sambot, I'm here to help you to find an idea of activity. What can I do for you ?");
-        })
-        .catch(error => {
-          console.log(error);
-        });
+        if (session.userData.token) {
+            say_hello(session);
+        } else {
+            session.beginDialog('Login');
+        }
     }
 ]).triggerAction({
     matches: 'Greeting'
@@ -74,17 +117,21 @@ bot.dialog('Greeting', [
 
 bot.dialog('Events', [
     function (session, args, next) {
-        var event_place_entity = botbuilder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.geography.city');
-        var event_keyword_entity = botbuilder.EntityRecognizer.findEntity(args.intent.entities, 'event_keyword');
-        var event_place = event_place_entity ? '&location.address='+event_place_entity.entity : '';
-        var event_keyword = event_keyword_entity ? '&q='+event_keyword_entity.entity : '';
-        axios.get(start_url + 'events/search/?token=' +  user_token + "&sort_by=date" + event_place + event_keyword)
-        .then(response => {
-            create_event_card(session, response);
-        })
-        .catch(error => {
-          console.log("err: "+ error);
-        });
+        if (session.userData.token) {
+            var event_place_entity = botbuilder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.geography.city');
+            var event_keyword_entity = botbuilder.EntityRecognizer.findEntity(args.intent.entities, 'event_keyword');
+            var event_place = event_place_entity ? '&location.address='+event_place_entity.entity : '';
+            var event_keyword = event_keyword_entity ? '&q='+event_keyword_entity.entity : '';
+            axios.get(start_url + 'events/search/?token=' +  session.userData.token + "&sort_by=date" + event_place + event_keyword)
+            .then(response => {
+                create_event_card(session, response);
+            })
+            .catch(error => {
+            console.log("err: "+ error);
+            });
+        } else {
+            session.beginDialog('Login');
+        }
     }
 ]).triggerAction({
     matches: 'Events'
@@ -93,31 +140,34 @@ bot.dialog('Events', [
 var categories_hash = {};
 bot.dialog('Categories', [
     function (session, args) {
-        axios.get(start_url + 'categories/?token=' +  user_token)
-        .then(response => {
-            response.data.categories.forEach(function(value){
-                categories_hash[value.name] = {id: value.id}
+        if (session.userData.token) {
+            axios.get(start_url + 'categories/?token=' +  session.userData.token)
+            .then(response => {
+                response.data.categories.forEach(function(value){
+                    categories_hash[value.name] = {id: value.id}
+                });
+                botbuilder.Prompts.choice(session, "Which kind of event could interest you ?", categories_hash, { listStyle: botbuilder.ListStyle.button });
+            })
+            .catch(error => {
+            console.log("err: "+ error);
             });
-            botbuilder.Prompts.choice(session, "Which kind of event could interest you ?", categories_hash, { listStyle: botbuilder.ListStyle.button });
-        })
-        .catch(error => {
-          console.log("err: "+ error);
-        });
+        } else {
+            session.beginDialog('Login');
+        }
     },
     function (session, results) {
-        if (results.response) {
+        if (results.response.entity) {
             var category = categories_hash[results.response.entity];
-            axios.get(start_url + 'events/search/?token=' +  user_token + "&categories=" + category.id)
+            axios.get(start_url + 'events/search/?token=' +  session.userData.token + "&categories=" + category.id)
             .then(response => {
                 create_event_card(session, response);
             })
             .catch(error => {
-              console.log("err: "+ error);
+                console.log("err: "+ error);
             });
-
-            session.send(); 
         }
     }
 ]).triggerAction({
     matches: 'Categories'
 });
+
